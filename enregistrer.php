e068dc27185522da84b80a0114785e9a843f78cc
Soukainabrando <78022433+Soukainabrando@users.noreply.github.com>
2021-02-05 21:42:00 +0100

    Delete enregistrer.php

--- enregistrer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nom=addslashes($_POST['nom_prenom']);
-$email=addslashes ($_POST['mail']);
-$date=array($_POST['jours'],$_POST['months'],$_POST['annees']);
-$date_naissance=implode("/",$date);
-$link=mysqli_connect("localhost","root","","glowfe") or die("Echec de connexion à la base");
-$sql="INSERT INTO `user`(`nom_prenom`,`mail`,`date_naissance`) VALUES (`$nom`,`$email`,`$date_naissance`)";
-$result=mysqli_query($link,$sql);
-header('Location: page4.php');?>
