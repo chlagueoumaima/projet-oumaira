a63dd987d9409bce5b14a298ceecc662001e3f4e
Fatima-ezzah <78173079+Fatima-ezzah@users.noreply.github.com>
2021-02-05 21:45:26 +0100

    Delete cookie.php

--- cookie.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nom_prenom=$_POST['nom_prenom'];
-$mail=$_POST['mail'];
-$temps=365*24*3600;
-setcookie("nom",$nom_prenom,time()+$temps);
-setcookie("email",$mail,time()+$temps);
-header('location: page2.php');
-?>
