d8d8320d9cbe03d210b5f63cf3d28a01e45f9b5c
Fatima-ezzah <78173079+Fatima-ezzah@users.noreply.github.com>
2021-03-15 15:55:40 +0100

    Add files via upload

---
+++ centre_assistance.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Centre d'assistance</title>
+	<meta charset="utf-8"/>
+	<link href="bootstrap4/css/bootstrap.min.css"
+rel="stylesheet" type="text/css">
+	<style type="text/css">
+      p{
+      	text-align:justify;
+      }
+      a{
+      	text-decoration: none;
+      	font-size: 20px;
+      }
+	</style>
+</head>
+<body bgcolor="#2874A6" TEXT="white" vlink="white" alink="white" link="white">
+
+	<h1 align="center">Bonjour,comment pouvons‑nous vous aider ?</h1>
+	<br/>
+	<ul type="square">
+            <a href="#utilisation"><li>Utiliser App</li></a><br/>
+            <a href="#compte"><li>Gérer votre compte</li></a><br/>
+            <a href="#securite"><li>Sécurité et protection</li></a><br/>
+            <a href="#regles"><li>Règles et politiques</li></a><br/>
+    </ul>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+
+<h1 align="center"><a name="utilisation">Utiliser App</a></h1>
+<hr width="75%">
+<p>
+Vous pouvez entamer une conversation privée avec n'importe lequel de vos abonnés ou créer une conversation de groupe avec des utilisateurs qui vous suivent.
+Toute personne que vous ne suivez pas peut vous envoyer un Message Privé si :
+vous avez choisi de recevoir des Messages Privés de la part de n'importe quel utilisateur ;
+ou vous avez précédemment envoyé un Message Privé à cette personne.
+Tout participant à une conversation peut envoyer des Messages Privés au groupe. Chaque utilisateur d'un groupe peut voir tous les messages, même si tout le monde ne se suit pas mutuellement.
+Dans une conversation de groupe, chaque participant peut en ajouter d'autres. Les participants nouvellement ajoutés ne voient pas l'historique de la conversation.
+Certains comptes, notamment des entreprises présentes sur Izone, ont activé un paramètre leur permettant de recevoir des Messages Privés de tous les utilisateurs. Vous pouvez donc adresser un Message Privé à ces comptes même s'ils ne vous suivent pas.
+Dans une conversation à deux ou une conversation de groupe, vous ne pouvez pas échanger avec un compte que vous avez bloqué.</p>
+<p>
+Si vous suivez quelqu'un sur cette application :
+<ul><li>Vous êtes abonné à ses Tweets.</li>
+<li>Ses Tweets figurent dans votre fil d'actualités.</li>
+<li>Cette personne peut vous envoyer des Messages Privés.</li></ul></p>
+
+<h2 style="color: black;">Qu'est ce qu'un abonné</h2>
+
+<p>
+Vos abonnés sont les personnes qui reçoivent vos Tweets. Si quelqu'un s'abonne à votre compte, ou vous suit :
+il apparaît dans votre liste d'abonnés.
+vos Tweets figurent dans son fil d'actualités quand il se connecte sur Izone.
+vous pouvez avoir une conversation privée avec lui.</p>
+
+<h2 style="color: black;">Comment savoir à quels comptes je suis abonné ?</h2>
+
+<p>Cliquez sur Abonnements sur votre page de profil ou d'accueil pour voir qui vous suivez, sous la forme d'une liste d'abonnements. Vous pouvez vous désabonner de comptes depuis cette page si vous ne souhaitez plus les suivre.
+</p>
+
+<h1 align="center"><a name="compte">Gérer votre compte</a></h1>
+<hr width="75%"> 
+	<h2 style="color: black;">Comment accéder à vos données?</h2>
+	<p>La consultation de vos données peut vous renseigner sur le type d'informations stockées au sujet de votre compte. Il s'agit d'un moyen facile de consulter les détails relatifs à votre compte et d'y apporter des modifications si nécessaire.</p>
+	<h2 style="color: black;">Quelles sont les informations à ma disposition?</h2>
+	<p><span style="font-weight: bold;color:grey;">Compte :</span> si vous êtes connecté à votre compte, vous verrez des informations telles que votre nom d'utilisateur et les adresses email et numéros de téléphone associés à votre compte, ainsi que les détails relatifs à la création de votre compte. Vous verrez également certaines informations que vous nous avez précédemment fournies, telles que la date de votre anniversaire. Que vous soyez connecté ou non, vous pouvez aussi voir certaines informations que nous avons déduites au sujet de votre compte ou de votre appareil,votre tranche d'âge. Vous pouvez mettre à jour ou corriger la plupart de ces informations à tout moment (les détails relatifs à la création de votre compte ne peuvent être modifiés).</p>
+	<p><span style="font-weight: bold;color: grey;">Historique du compte :</span> si vous êtes connecté, vous aurez également accès à votre historique de connexion et à une liste des lieux où vous avez utilisé cette application. </p>
+
+<h1 align="center"><a name="securite">Sécurité et protection</a></h1>
+<hr width="75%">
+	<h2 style="color: black;">Comment protéger vos informations personnelles?</h2>
+	<p>Pour protéger efficacement vos informations, il est primordial de connaître et de maîtriser ces paramètres. Les articles suivants décrivent comment préserver au mieux la confidentialité de votre compte : 
+    <ul><li>Maîtriser votre expérience</li>
+    <li>Protéger vos Tweets</li>
+    <li>Sécuriser votre compte</li></ul></p>
+    <h2 style="color: black;">Pensez à ce que vous tweetez?</h2>
+    <p>
+C'est vous qui contrôlez le contenu partagé ou tout autre site Web. Ne publiez jamais d'informations considérées comme confidentielles.
+Méfiez‑vous des communications qui vous demandent vos coordonnées, données personnelles ou mots de passe. Si vous avez un doute avant de tweeter, nous vous conseillons de vous poser les questions suivantes :</p>
+<ul type="circle">
+<li>Avec qui mes informations sont‑elles partagées ?</li>
+<li>Quel type et quelle quantité d'informations suis‑je sur le point de partager ?</li>
+<li>À combien d'utilisateurs mes informations sont‑elles accessibles ?</li>
+<li>Puis‑je me fier à l'ensemble des personnes qui disposent d'un accès à ces informations ?</li></ul>
+<p>La notion de confidentialité est spécifique à chaque individu. Chacun est libre de partager ce qu'il veut. Si l'un de vos amis ou une connaissance a publié des informations que vous jugez confidentielles, contactez cette personne et demandez‑lui de retirer le contenu. De la même manière, respectez le souhait des autres utilisateurs. Si, après avoir publié des informations relatives à un autre utilisateur, ce dernier vous demande de les retirer, respectez sa demande. Cet article explique comment supprimer un Tweet.</p>
+    		
+<h1 align="center"><a name="regles">Règles et politiques</a><br/>
+<hr width="75%">
+	<h1 style="color: black;">Quels sont les types de réclamations relatives à des droits d'auteur traités par cette application?</h1>
+	<p>Cette application répond aux réclamations relatives à des droits d'auteur envoyées dans le cadre de l'application de la loi Digital Millennium Copyright Act (DMCA). La Section 512 de la loi DMCA présente les exigences légales à respecter pour signaler de manière formelle une violation de droits d'auteur. Elle donne également des instructions sur la façon dont une partie concernée peut faire appel d'une suppression en envoyant un avis de contestation conforme.</p>
+<p>
+Cette application répond aux signalements de violation de droit d'auteur présumée, tels que les allégations relatives à l'utilisation non autorisée d'une image protégée en tant que photo de profil ou de bannière, à l'utilisation non autorisée d'une vidéo ou d'une image protégée importée via nos services d'hébergement multimédias, ou à des Tweets contenant des liens vers des ressources présentant des violations présumées. Notez que toutes les utilisations non autorisées d'éléments protégés par des droits d'auteur ne constituent pas des infractions (reportez‑vous à notre article sur l'usage raisonnable pour plus d'informations).</p>
+<p>
+Si vous vous inquiétez de l'utilisation du nom de votre marque ou entité, veuillez consulter la Politique en matière de marques déposées de Cette application. Si vos préoccupations sont liées à un compte parodique, de flux d'actualités, de commentaires ou de fan, veuillez consulter la politique à ce sujet ici. Ces questions ne relèvent en général pas de problèmes de droits d'auteur.</p>
+ 		
+		
+  
+
+</body>
+</html>
