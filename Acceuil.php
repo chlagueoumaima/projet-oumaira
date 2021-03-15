2d4396242a00f0b191d4e785d2b39814d2c79c41
chlagueoumaima <78088815+chlagueoumaima@users.noreply.github.com>
2021-03-15 20:54:46 +0100

    Update Acceuil.php

--- Acceuil.php
+++ Acceuil.php
@@ -1,7 +1,14 @@
+            <?php
+                session_start();
+                header('Content-Type: text/html; charset=iso-8859-1');
+                ?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
 	<meta charset="utf-8">
+           <link rel="stylesheet" type="text/css" href="styleM.css">
+
 	<title>          </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -17,19 +24,20 @@
   display: block; 
   padding: 12px; 
   text-decoration: none;}
-  .menu a:hover{background-color:#DCDCFF;}
-  .menu a.home{background-color:#DCDCFF; 
+  .menu a:hover{background-color:#72E55B;
+    color: white;}
+  .menu a.home{background-color:#72E55B; 
   color: white;}
   .button{ display: block;
   width: 14.9%;
   border-radius: 15px;
   color: white;
-background-color:#3C3CEB;}
+background-color:#72E55B;}
 .head{position: fixed;
   bottom: 90%;
   right: 17%;
   width: 60%;
-  border: 1px grey;}
+  border: 0px white;}
   .vertical-line{
         border-left: 1px solid grey;
         display: inline-block;
@@ -37,8 +45,7 @@ background-color:#3C3CEB;}
         margin-left: 20%;
         position: absolute;
       }
-.img{
-	}
+
 	
 
 
@@ -54,10 +61,10 @@ background-color:#3C3CEB;}
 	<h5>Acceuil</h5>
 	<hr>
             <?php
-                session_start();
+                
                 if($_SESSION['mail'] !== "" && $_SESSION['mail'] !== "" ){
-                        $link = mysqli_connect("localhost","root","","glowfe")
-           or die('could not connect to database');
+                             include ("connexion.php");
+
     
         $requete = "SELECT * FROM user where  mail = '".$_SESSION['mail']."' ";       
         $exec_requete = mysqli_query($link,$requete);
@@ -68,15 +75,44 @@ background-color:#3C3CEB;}
 
           
                     
-                    // afficher un message
-                  echo  $reponse['nom_prenom'];
-                   echo "<img src=\"photo/$photo\" alt=\"image\" height=40 width=40/>";
+                    
+        echo "<img class=pseu src=\"photo/$photo\" alt=\"image\" height=50 width=50/>";
+                  echo '<span style="font-size:20px;font-weight=bold;padding=5px;"  >'. $reponse['nom_prenom'].'</span>';
+                   
 
 
                 }
             ?>
 </div>
-<div>
+<div class="post">
+  <?php
+// on se connecte à notre base
+include('connexion.php');
+
+$sql = "SELECT  texte_news,nom_prenom FROM news,user WHERE  news.id_user=user.id  ";
+$result=mysqli_query($link,$sql);
+
+
+$nb_news = mysqli_num_rows($result);
+if ($nb_news == 0) {
+  echo 'Aucune news enregistrée.';
+}
+else {
+  // si on a au moins une news, on l'affiche
+  while ( $data = mysqli_fetch_array($result))
+   {
+  echo  '<span style="font-size:20px;font-weight=bold;padding=5px;color:#006400;"  >'.$data['nom_prenom'].'</span>' ;
+  echo "</br>";
+
+
+  echo  '<span style="font-size:15px;padding=5px;"  >'.$data['texte_news'].'</span>' ;
+  echo "</br>";
+    echo "_____________________________________________________________________________________________________";
+    echo "</br>";
+  }
+}
+
+?>
 </div>
 <div class="center">
 	</div>
@@ -87,14 +123,14 @@ background-color:#3C3CEB;}
 	<a href="#"><i class="material-icons" style="top:8px;position: relative">explore</i>Explorer</a>
 	<a href="#"><i class="material-icons" style="top:8px;position: relative">notifications</i>Notifications</a>
 	<a href="messagerie.php"><i class="material-icons"style="top:8px;position: relative"style="top:8px;position: relative">message</i>Messages</a>
-	<a href="abonne.php"><i class="material-icons"style="top:8px;position: relative">bookmark_border</i>Signets</a>
+	<a href="abo.php"><i class="material-icons"style="top:8px;position: relative">bookmark_border</i>Signets</a>
 	<a href="follow.php"><i class="material-icons"style="top:8px;position: relative">list_alt</i>Listes</a>
-	<a href="#"><i class="material-icons"style="top:8px;position: relative">person</i>Profil</a>
+	<a href="profil.php"><i class="material-icons"style="top:8px;position: relative">person</i>Profil</a>
 	<a href="#"><i class="material-icons"style="top:8px;position: relative">add_circle_outline</i>Plus</a>
     <a href="deconnection.php"><i class="material-icons"style="top:8px;position: relative">lock</i>se deconnecter</a>
 
 </div>
-<button class="button">Tweeter</button>
+<button class="button"><a style="text-decoration: none;color: white;" href="Insert_news.php">Publier</a></button>
 </nav>
 
 
